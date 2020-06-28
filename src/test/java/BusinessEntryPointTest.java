1ad94eaaea5560c50a8ab671cfd4f79a6d441fd8
m0k00i6 <manoj.kumar0@walmartlabs.com>
2020-06-28 00:53:29 -0500

    added gitignore file and dummy test resources

---
+++ src/test/java/BusinessEntryPointTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BusinessEntryPointTest {
+
+    @Test
+    public void test() {
+        log.info("Dummy test...");
+    }
+}
